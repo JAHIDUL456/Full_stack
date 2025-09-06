aa3243b491ebbb10876d9d62453ab41de194177b
JAHIDUL456 <mdjahidulislamjahid1n1@gmail.com>
2025-09-06 11:31:27 +0600

    jaki

--- backend/script.js
+++ backend/script.js
@@ -1,6 +1,12 @@
-const express =require('express');
-const app= express();
+import express from "express";
+import cors from "cors";
+const app=express();
+// Enable CORS for frontend
+app.use(cors({
+  origin: 'http://localhost:5173', // allow requests from your frontend
+}));
 
+app.use(express.json());
 app.get('/',(req,res)=>{
     res.json({
         id:1,
