5cf775dcbb23c6f6961387b40046cc1e6d62e7cf
JAHIDUL456 <mdjahidulislamjahid1n1@gmail.com>
2025-09-13 18:14:15 +0600

    jaki

--- backend/script.js
+++ backend/script.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 const app=express();
+app.set('view engine','ejs');
 // Enable CORS for frontend
 app.use(cors({
   origin: 'http://localhost:5173', // allow requests from your frontend
@@ -35,7 +36,33 @@ app.get('/profile/:username',(req,res,next)=>{
 
 
 
+app.get('/about/:user',(req,res)=>{
+  const {user}=req.params;
+  res.send(`this is about page of ${user}`);
 
+})
 
+app.get('/home',(req,res)=>{
+  res.render('home');
+})
+
+//new api in town 
+
+app.get('/data',(req,res)=>{
+  res.json([{
+    id:2,
+    name:"Rafi",
+    roll:30,
+    city:"Chittagong",
+    country:"Bangladesh"
+  },
+{
+    id:3,
+    name:"Rafiq",
+    roll:20,
+    city:"Khulna",
+    country:"Bangladesh"
+}])
+})
 
 app.listen(5000);
